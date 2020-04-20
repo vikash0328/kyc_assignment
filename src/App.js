7c56793e5897824f7ced12f1d9b031322ee96dbb
vikash0328 <vikash.arya201@gmail.con>
2020-04-21 01:02:45 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      hi i am vikas kumar and i am 3rd year undergraduate student of IIT kharagpur
+    </div>
+  )
+}

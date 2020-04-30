24a30b9ad0e46143e8cbe402cecc40cdcca98381
ptkumar8 <41840411+ptkumar8@users.noreply.github.com>
2020-04-30 16:09:41 +0530

    update in Preview page

--- src/Components/Docs.js
+++ src/Components/Docs.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import history from "../history";
 import { withRouter } from 'react-router-dom';
-// import './App.css';
-
-
+import "../Css/Docs.css";
+import { green } from '@material-ui/core/colors';
+import Card from "@material-ui/core/Card";
+import Container from '@material-ui/core/Container';
 class Preview extends React.Component{
     constructor(props)
     {
@@ -28,35 +29,61 @@ class Preview extends React.Component{
           window.location.reload();
       }
       ev.preventDefault();
-    };
+    }
     render(){
         return(
-         <div className='App-header ' style={{color:'green'}}>
-             <hr style={{width:'60%'}}/>
-             <div><strong>Preview Page</strong></div>
-             <hr style={{width:'60%'}}/>
-             <div>Personal Information:</div>
-             <div>NAME:{this.state.name}</div>
-             <div>DATE OF BIRTH: {this.state.dateofbirth}</div>
-             <div>GENDER:{this.state.gender}</div>
-             <hr style={{width:'80%'}}/>
-              <div>Selfie:</div>
-             <div><img src={this.state.img} alt='Selfie'></img></div>
-             <hr style={{width:'80%'}}/>
-             <div>Document:</div>
-             <div> Name of Goverment ID:{this.state.id}</div>
-             <hr style={{width:'60%'}}/>
-             <div>Front side Image of {this.state.id}</div>
-             <div><img src={this.state.imgfront} alt='front side of document'></img></div>
-             <hr style={{width:'80%'}}/>
-             <div>Back side Image of {this.state.id}</div>
-             <div><img  src={this.state.imgback} alt='back side of document'></img></div>
-             <hr style={{width:'80%'}}/>
-             <div><button type='submit' name='submit'  onClick={this.handleonsubmit}>SUBMIT</button></div>
 
-         </div>
+        <div className="App-header">
+                <Container id="PageContainer">
+          
+                
+            
+
+
+
+    <div id="DocsBox" >
+       <card >
+           <cardcontent id="DocsHeading"> 
+           <strong>Preview Page</strong>
+           </cardcontent>
+       </card>
+       
+       <hr style={{width:'60%'}}/>
+       <card>
+           <cardcontent>
+             <div><strong>Name:</strong >{this.state.name}</div>
+             <div><strong>Date Of Birth:</strong> {this.state.dateofbirth}</div>
+             <div><strong>Gender:</strong>{this.state.gender}</div>
+           </cardcontent>
+       </card>                     
+   </div>
+
+   <div id="DocsBox">
+  
+        <div id="DocsSelfieHeading"><strong>Selfie:</strong></div>
+        <hr style={{width:'60%'}}/>
+        <div><img src={this.state.img} id="DocsImage" alt='Selfie'></img></div>
+        
+    </div> 
+
+    <div id="DocsBox">   
+        
+       <div id="DocsSelfieHeading"> <strong>Goverment ID:</strong>{this.state.id}</div>
+            
+       <hr style={{width:'60%'}}/>
+    
+         <div><img src={this.state.imgfront}  id="DocsImage" alt='front side of document'></img></div>
+        <div>Front side Image of {this.state.id}</div>
+
+        <div><img  src={this.state.imgback}  id="DocsImage" alt='back side of document'></img></div>
+        <div>Back side Image of {this.state.id}</div>
+        <div><button type='submit' name='submit'  onClick={this.handleonsubmit}>SUBMIT</button></div>
+   </div>
+
+   </Container>
+      </div>
         );
-    };
+}
 
 }
 export default withRouter(Preview);

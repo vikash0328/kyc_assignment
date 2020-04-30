95c7d3843970b3bbfcafff318d1dbea212aa471f
ptkumar8 <41840411+ptkumar8@users.noreply.github.com>
2020-04-30 17:29:19 +0530

    Update of review Page

--- src/Components/Docs.js
+++ src/Components/Docs.js
@@ -1,23 +1,21 @@
 import React from 'react'
-import history from "../history";
 import { withRouter } from 'react-router-dom';
-import "../Css/Docs.css";
-import { green } from '@material-ui/core/colors';
-import Card from "@material-ui/core/Card";
 import Container from '@material-ui/core/Container';
+import '../Css/Docs.css';
+import { green } from '@material-ui/core/colors';
+
 class Preview extends React.Component{
     constructor(props)
     {
         super(props);
          this.state={
-            name:sessionStorage.getItem('Name'),
-            dateofbirth:sessionStorage.getItem('DOB'),
-            gender:sessionStorage.getItem('gender'),
-            img:sessionStorage.getItem('img'),
-            imgfront:sessionStorage.getItem('front'),
-            imgback:sessionStorage.getItem('back'),
-            id:sessionStorage.getItem('id')
-
+             name:'XYZ',
+             dateofbirth:'08/05/1998',
+             gender:'MALE',
+             img:sessionStorage.getItem('img'),
+             imgfront:sessionStorage.getItem('front'),
+             imgback:sessionStorage.getItem('back'),
+             id:sessionStorage.getItem('id')
          }
         
     }
@@ -27,65 +25,69 @@ class Preview extends React.Component{
       {
     //   {  console.log(this.state.imgfront.length);
           alert('FORM IS SUBMITTED ')
-          history.push("/");
-          window.location.reload();
       }
       ev.preventDefault();
     }
     render(){
         return(
+<div id="DocsMain">
 
-        <div className="App-header">
-                <Container id="PageContainer">
           
-                
-            
-
-
-
-    <div id="DocsBox" >
-       <card >
+<div id="DocsBox" >
+       <card>
            <cardcontent id="DocsHeading"> 
            <strong>Preview Page</strong>
            </cardcontent>
        </card>
        
-       <hr style={{width:'60%'}}/>
+    <hr style={{width:'60%'}}/>
        <card>
+           
            <cardcontent>
              <div><strong>Name:</strong >{this.state.name}</div>
              <div><strong>Date Of Birth:</strong> {this.state.dateofbirth}</div>
              <div><strong>Gender:</strong>{this.state.gender}</div>
+
            </cardcontent>
        </card>                     
    </div>
-
    <div id="DocsBox">
   
+
         <div id="DocsSelfieHeading"><strong>Selfie:</strong></div>
-        <hr style={{width:'60%'}}/>
         <div><img src={this.state.img} id="DocsImage" alt='Selfie'></img></div>
-        
+        <hr style={{width:'80%'}}/>
+
     </div> 
 
     <div id="DocsBox">   
-        
-       <div id="DocsSelfieHeading"> <strong>Goverment ID:</strong>{this.state.id}</div>
-            
-       <hr style={{width:'60%'}}/>
+ 
+    <div id="DocsSelfieHeading"> <strong>Goverment ID:</strong>{this.state.id}</div>
+    <hr style={{width:'60%'}}/>
     
-         <div><img src={this.state.imgfront}  id="DocsImage" alt='front side of document'></img></div>
-        <div>Front side Image of {this.state.id}</div>
+    <div><img src={this.state.imgfront} id="DocsImage" alt='front side of document'></img></div>
+    <div>Front side Image of {this.state.id}</div>
 
-        <div><img  src={this.state.imgback}  id="DocsImage" alt='back side of document'></img></div>
-        <div>Back side Image of {this.state.id}</div>
-        <div><button type='submit' name='submit'  onClick={this.handleonsubmit}>SUBMIT</button></div>
+   
+    <div><img  src={this.state.imgback} id="DocsImage" alt='back side of document'></img></div>
+    <div>Back side Image of {this.state.id}</div>
+
+    <div><button type='submit' name='submit'  onClick={this.handleonsubmit}>SUBMIT</button></div>
+
+
+
+
+
    </div>
 
-   </Container>
-      </div>
+
+
+  
+</div>
+ 
+
         );
-}
+    }
 
 }
 export default withRouter(Preview);
